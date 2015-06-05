613303648c84a2ae1db81e8c473fd4924ddf25c1
peel3r <p@weljakubowski.com>
2015-06-05 10:38:36 +0100

    new reader view spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/register' , to: 'readers#new' , as: 'register'
+
+  resources :readers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
